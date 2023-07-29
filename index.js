da8edcf325b7b335a3e1b4d8a9ea6a62a6726bc7
Daniil M <41665964+DANLEP@users.noreply.github.com>
2023-07-29 15:53:50 +0300

    Added MongoCluster & User model

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log("hello world")
+const express = require("express");
+const mongoose = require("mongoose");
+const config = require("config");
+
+const app = express();
+const PORT = config.get("serverPort");
+
+const start = async () => {
+    try{
+        mongoose.connect(config.get("dbURL"))
+        app.listen(PORT, () => {
+            console.log('Server started on port ', PORT)
+        })
+    } catch(e) {
+
+    }
+}
+
+start();
